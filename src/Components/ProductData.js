defd000765d980881c46b0508f82baed87bc76dd
LeonGR6 <layormkarnales@gmail.com>
2023-11-08 12:01:01 -0600

    Agregue 10 productos

--- src/Components/ProductData.js
+++ src/Components/ProductData.js
@@ -1,33 +1,83 @@
 import VictusImage from '../img/Victus.png';
-
-
+import Marka from '../img/marcadores.png'
+import Cuade from '../img/cuadernos.png'
+import Oxford from '../img/Cuadernosoxford.png'
+import Mous from '../img/mouse.png'
+import Lenovo from '../img/lenovo.png'
+import Boli from '../img/boligrafo.png'
+import Moro from '../img/mochak.png'
+import Sarp from '../img/sharpies.png'
+import Usb from '../img/usbc.png'
 
 
 const productsData = [
     {
       imageSrc: VictusImage,
-      title: 'Producto 1',
-      price: '$100.00',
-      description: 'Descripción del producto 1'
+      title: 'Laptop Gamer Hp Victus',
+      price: '$14,000.00',
+      description: '15.6, Intel Core i5-12450h'
     },
     {
-        imageSrc: VictusImage,
-        title: 'Producto 1',
-        price: '$100.00',
-        description: 'Descripción del producto 1'
+        imageSrc: Marka,
+        title: 'Marcadores Okasis',
+        price: '$138.00',
+        description: 'Marcadores punta pincel 30 colores'
       },
       {
-        imageSrc: VictusImage,
-        title: 'Producto 1',
-        price: '$100.00',
-        description: 'Descripción del producto 1'
+        imageSrc: Cuade,
+        title: 'Diario profesional',
+        price: '$460.90',
+        description: 'Cuaderno profesional 10.5 x 7.5 pulgadas.'
       },
       {
-        imageSrc: VictusImage,
-        title: 'Producto 1',
-        price: '$100.00',
-        description: 'Descripción del producto 1'
+        imageSrc: Oxford,
+        title: 'Cuaderno Oxford',
+        price: '$293.00',
+        description: 'Cuaderno de papel color verde 60 hojas'
       },
+      {
+        imageSrc: Mous,
+        title: 'Mouse Gaming',
+        price: '$1,499.00',
+        description: 'Logitech G305 LIGTHSPEED Inalambrico'
+      },
+      {
+        imageSrc: Lenovo,
+        title: 'Laptop Lenovo IdeaPad',
+        price: '$3,665.00',
+        description: '14 p, Intel Celeron N4020'
+      },
+      {
+        imageSrc: Boli,
+        title: 'Boligrafo Paper',
+        price: '$70.00',
+        description: 'Paper Mate Kilométrico Punta Fina Tinta Negra'
+      },
+      {
+        imageSrc: Moro,
+        title: 'Mochila Moro',
+        price: '$459.00',
+        description: 'Mochila para hombre Mochila para portátil de viaje'
+      },
+      {
+        imageSrc: Sarp,
+        title: 'harpie S-Gel',
+        price: '$171.00',
+        description: 'bolígrafos de gel, punta media (0,7 mm)'
+      },
+      {
+        imageSrc: Usb,
+        title: 'Cable USB',
+        price: '$199.00',
+        description: 'Cable usb c carga rapida(1M 2 Unidades)'
+      },
+     
+
+
+
+
+
+
     // ...otros productos
   ];
   
